9d337d3e1cd0f4d7b33ebc348197fe934b3f1d41
TechNik <aniket.97.mane@gmail.com>
2023-06-19 01:16:04 +0530

    Create struct for Vec3 and implemented it.

---
+++ src/core/vec3.rs
@@ -0,0 +1,27 @@
+
+// Vec3 struct represents vector in 3D space
+// We're using x, y and z coordinates for location.
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        // Create a new Vector
+        Self { x, y, z}
+    }
+
+    pub fn x(&self) -> f32 {
+        self.x
+    }
+
+    pub fn y(&self) -> f32 {
+        self.y
+    }
+
+    pub fn z(&self) -> f32 {
+        self.z
+    }
+}
