2fe3b16df1e6fd8b0583f549dfbdd49ec7f52c56
TechNik <aniket.97.mane@gmail.com>
2023-06-19 18:14:49 +0530

    Implemented remaining vector fn like dot and cross product, length, unit vector etc.

--- src/core/vec3.rs
+++ src/core/vec3.rs
@@ -1,6 +1,7 @@
 
 // Vec3 struct represents vector in 3D space
 // We're using x, y and z coordinates for location.
+#[derive(Debug)]
 pub struct Vec3 {
     x: f32,
     y: f32,
@@ -23,5 +24,38 @@ impl Vec3 {
 
     pub fn z(&self) -> f32 {
         self.z
+    }
+
+    pub fn length_squared(&self) -> f32 {
+        (self.x() * self.x()) + (self.y() * self.y()) + (self.z() * self.z()) 
+    }
+
+    pub fn length(&self) -> f32 {
+        // returns the length of the vector
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot_product(u: &Vec3, v: &Vec3) -> f32 {
+        // Dot product of 2 vectors.
+        // About Dot product: https://www.mathsisfun.com/algebra/vectors-dot-product.html
+        (u.x() * v.x()) + (u.y() * v.y()) + (u.z() + v.z())
+    }
+
+    pub fn cross_product(u: &Vec3, v: &Vec3) -> Self {
+        // Cross product of 2 vectors.
+        // About Cross product: https://www.mathsisfun.com/algebra/vectors-cross-product.html
+        let x = u.y() * v.z() - u.z() * v.y();
+        let y = u.z() * v.x() - u.x() * v.z();
+        let z = u.x() * v.y() - u.y() * v.x();
+
+        Self { x, y, z }
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        // Returns unit vector of the given vector
+        // About unit vector: https://www.mathsisfun.com/algebra/vector-unit.html
+        let length = self.length();
+
+        Self::new(self.x / length, self.y / length, self.z / length)
     }
 }
