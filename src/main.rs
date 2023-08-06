ea65798b8fee967ac1920391198848cf839796d7
TechNik <aniket.97.mane@gmail.com>
2023-08-06 21:20:03 +0530

    Using values from configuration

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 use ray_tracer::core::{ray::Ray, vec3::Vec3, colour::colour};
+use ray_tracer::configuration::get_configuration;
+
 
 fn main() {
-    let width = 360;
-    let height = 240;
+    let config = get_configuration().expect("Couldn't read configuration");
+
+ 
+    let aspect_ratio = 16.0 / 9.0;
+
+    let width = 400;
+    let height = width as f32 / aspect_ratio;
     let max = 255;
 
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    // println!("{} : {}", width, height);
     println!("P3\n{} {}\n{}", width, height, max);
 
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
+    
 
-
-    for j in (0..height).rev() {
+    for j in (0..height as i32).rev() {
         for i in 0..width {
             let u = i as f32 / width as f32;
             let v = j as f32 / height as f32;
 
-            let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+            let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v - origin);
             let clr = colour(&ray);
 
             // let b: f32 = 0.25;
