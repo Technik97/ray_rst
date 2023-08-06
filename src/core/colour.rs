e8ea02109a4abe253b58bbc4741ac353a555b380
TechNik <aniket.97.mane@gmail.com>
2023-08-06 21:17:56 +0530

    Added logic for unit length vector N for shading

--- src/core/colour.rs
+++ src/core/colour.rs
@@ -1,7 +1,7 @@
 use super::ray::Ray;
 use super::vec3::Vec3;
 
-pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> bool {
+pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin() - *center;
     let a = Vec3::dot_product(&ray.direction, &ray.direction);
     let b = 2.0 * Vec3::dot_product(&oc, &ray.direction);
@@ -9,12 +9,20 @@ pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> bool {
 
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / 2.0 * a;
+    }
 }
 
 pub fn colour(ray: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t =  hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+    
+    if t > 0.0 {
+        let v = ray.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0);
+        let N = v.unit_vector();
+        return Vec3::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0) * 0.5;
     }
 
     let direction: Vec3 = ray.direction();
