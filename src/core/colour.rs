6ddc4776f61ad357c06fd5c9a70e27181293ba58
TechNik <aniket.97.mane@gmail.com>
2023-08-06 16:32:02 +0530

    Added code to generate a sphere

--- src/core/colour.rs
+++ src/core/colour.rs
@@ -1,7 +1,29 @@
 use super::ray::Ray;
 use super::vec3::Vec3;
 
+fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
+    let origin_center = ray.origin() - *center;
+    let a = Vec3::dot_product(&ray.direction(), &ray.direction());
+    let b = 2.0 * Vec3::dot_product(&origin_center, &ray.direction());
+    let c = Vec3::dot_product(&origin_center, &origin_center) - radius * radius;
+
+    let discriminant = b*b - 4.0*a*c;
+
+    if discriminant < 0.0 {
+        return -1.0
+    } else {
+        return (-b - discriminant.sqrt()) / 2.0 * a;
+    }
+}
+
 pub fn colour(ray: &Ray) -> Vec3 {
+    let t =  hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let v = ray.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0);
+        let N = v.unit_vector();
+        return Vec3::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0) * 0.5;
+    }
+
     let direction: Vec3 = ray.direction();
     let v = direction.unit_vector();
     let unit_direction = v.unit_vector();
