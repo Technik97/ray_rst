71c5fc9bf8120a637f15dbe0c9490000940f62eb
TechNik <aniket.97.mane@gmail.com>
2023-08-28 01:11:55 +0530

    hit function now returns HitRecord

--- src/core/hittable.rs
+++ src/core/hittable.rs
@@ -12,8 +12,7 @@ pub struct HitRecord {
 pub type List = Vec<Box<dyn Hittable>>;
 
 pub trait Hittable {
-    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32, rec: &mut HitRecord) -> bool ;
-    // fn set_face_normal(self, ray: &Ray, outward_noraml: &Vec3);
+    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord> ;
 }
 
 pub struct HittableList {
@@ -27,19 +26,17 @@ impl HittableList {
 }
 
 impl Hittable for HittableList {
-    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32, rec:  &mut HitRecord) -> bool {
-        let mut tmp_rec = HitRecord::default();
-        let mut hit_anything = false;
+    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord> {
+        let mut record: Option<HitRecord> = None;
         let mut closest_so_far = tmax;
 
          for object in &self.list {
-            if object.hit(ray, tmin, closest_so_far, &tmp_rec) {
-                hit_anything = true;
-                closest_so_far = tmp_rec.t;
-                rec = &mut tmp_rec;
+            if let Some(rec) =  object.hit(ray, tmin, closest_so_far) {
+                closest_so_far = rec.t;
+                record = Some(rec);
             }
         }
 
-        hit_anything
+        record
     }
 }
