0173438aad315a4d5a79eecc74ec6916a3aaf3fc
TechNik <aniket.97.mane@gmail.com>
2023-08-28 00:03:51 +0530

    Added HittableList struct and implemented it

--- src/core/hittable.rs
+++ src/core/hittable.rs
@@ -9,7 +9,37 @@ pub struct HitRecord {
     pub front_face: bool
 }
 
+pub type List = Vec<Box<dyn Hittable>>;
+
 pub trait Hittable {
     fn hit(&self, ray: &Ray, tmin: f32, tmax: f32, rec: &mut HitRecord) -> bool ;
-    fn set_face_normal(self, ray: &Ray, outward_noraml: &Vec3);
+    // fn set_face_normal(self, ray: &Ray, outward_noraml: &Vec3);
+}
+
+pub struct HittableList {
+    list: List,
+}
+
+impl HittableList {
+    pub fn new(list: List) -> Self {
+        Self { list }
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32, rec:  &mut HitRecord) -> bool {
+        let mut tmp_rec = HitRecord::default();
+        let mut hit_anything = false;
+        let mut closest_so_far = tmax;
+
+         for object in &self.list {
+            if object.hit(ray, tmin, closest_so_far, &tmp_rec) {
+                hit_anything = true;
+                closest_so_far = tmp_rec.t;
+                rec = &mut tmp_rec;
+            }
+        }
+
+        hit_anything
+    }
 }
