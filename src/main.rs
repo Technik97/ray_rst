1d083014248a97c4ea6fe3149cd778c40683f03e
TechNik <aniket.97.mane@gmail.com>
2023-08-29 01:18:24 +0530

    rendering materials

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,31 @@ use rand::Rng;
 use ray_tracer::core::hittable::{HittableList, List};
 use ray_tracer::core::sphere::Sphere;
 use ray_tracer::core::{vec3::Vec3, colour::colour};
+use ray_tracer::materials::lambertian::Lambertian;
 use ray_tracer::materials::material::Material;
+use ray_tracer::materials::metal::Metal;
 use ray_tracer::setup::camera::Camera;
 
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
 
-    let width = 400;
+    let width = 720;
     let height = width as f32 / aspect_ratio;
     let max = 255;
     let samples = 100;
     let mut rng = rand::thread_rng();
 
     let camera = Camera::new();
-    let material = Material::default();
+    let material1 = Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.3, 0.3)));
+    let material2 = Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)));
+    let material3 = Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.5));
+    let material4 = Material::Metal(Metal::new(Vec3::new(0.8, 0.8, 0.8), 0.0));
 
     let mut list: List = Vec::new();
-    list.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, material)));
-    list.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material)));
+    list.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, material1)));
+    list.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material2)));
+    list.push(Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material3)));
+    list.push(Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material4)));
     let world = HittableList::new(list);
 
     // println!("{} : {}", width, height);
@@ -35,7 +42,7 @@ fn main() {
     
                 // let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v - origin);
                 let ray = camera.get_ray(u, v);
-                clr = clr + colour(&ray, &world, 100);
+                clr = clr + colour(&ray, &world, 0);
             }
             // let b: f32 = 0.25;
             clr = clr / samples as f32;
