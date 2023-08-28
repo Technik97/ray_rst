fa0ed17dc43f3bbe9ebd41b806a11b6fae5c45aa
Aniket <aniket.97.mane@gmail.com>
2023-08-28 13:37:07 +0530

    Added random_in_unit_sphere() function and modified colour function to add diffusion

--- src/core/colour.rs
+++ src/core/colour.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use super::hittable::{HittableList, Hittable};
 use super::ray::Ray;
 use super::vec3::Vec3;
@@ -19,11 +21,28 @@ pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
 
 pub fn colour(ray: &Ray, world: &HittableList) -> Vec3 {
     if let Some(rec) = world.hit(ray, 0.0, std::f32::MAX) {
-        return Vec3::new(rec.normal.x() + 1.0, rec.normal.y() + 1.0, rec.normal.z() + 1.0) * 0.5;
+        let target = rec.p + rec.normal + random_in_unit_sphere();
+
+        return colour(&Ray::new(rec.p, target - rec.p), &world) * 0.5;
     } else {
-        // let direction: Vec3 = ray.direction();
-        let t =  hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+        let unit_direction = Vec3::unit_vector(&ray.direction());
+
+        // let t =  hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+        let t = (unit_direction.y() + 1.0) * 0.5;
 
         return Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t;
+    }
+}
+
+pub fn random_in_unit_sphere() -> Vec3 {
+    let mut p = Vec3::default();
+    let mut rng = rand::thread_rng();
+
+    loop {
+        p = (Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>()) * 2.0) - Vec3::new(1.0, 1.0, 1.0);
+
+        if p.length_squared() < 1.0 {
+            return p;
+        }
     }
 }
