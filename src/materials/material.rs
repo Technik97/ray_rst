37ce088d7b5eac210abf26082dd0c418f2c69847
Aniket <aniket.97.mane@gmail.com>
2023-08-28 17:00:30 +0530

    Added Scatterable trait and Added materials to Materia Enum

--- src/materials/material.rs
+++ src/materials/material.rs
@@ -1,11 +1,21 @@
+use crate::core::{ray::Ray, hittable::HitRecord, vec3::Vec3};
+
+use super::{lambertian::Lambertian, metal::Metal, dielectric::Dielectric};
+
+
+#[derive(Clone, Copy)]
 pub enum Material {
-    Lambertian {},
-    Metal {},
-    Dielectric {}
+    Lambertian(Lambertian),
+    Metal(Metal),
+    Dielectric(Dielectric)
 }
 
 impl Default for Material {
     fn default() -> Self {
-        Self::Lambertian {  }
+        Self::Lambertian(Lambertian::default())
     }
+}
+
+pub trait Scatterable {
+    fn scatter(ray_in: &Ray, hit_record: HitRecord, attentuation: &mut Vec3, scattered: &mut Ray) -> bool;
 }
