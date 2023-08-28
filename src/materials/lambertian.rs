c05ea54aac3471cdaa42adcc6fb7b180b279aeda
Aniket <aniket.97.mane@gmail.com>
2023-08-28 20:40:08 +0530

    Implemented Scatterable for Lambertian

--- src/materials/lambertian.rs
+++ src/materials/lambertian.rs
@@ -1,4 +1,6 @@
-use crate::core::vec3::Vec3;
+use crate::core::{vec3::Vec3, ray::Ray, hittable::HitRecord, colour::random_in_unit_sphere};
+
+use super::material::Scatterable;
 
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Lambertian {
@@ -8,5 +10,15 @@ pub struct Lambertian {
 impl Lambertian {
     pub fn new(albedo: Vec3) -> Self {
         Self { albedo }
+    }
+}
+
+impl Scatterable for Lambertian {
+    fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord, attentuation: &mut Vec3, scattered: &mut Ray) -> bool {
+        let target = hit_record.p + hit_record.normal + random_in_unit_sphere();
+        *scattered = Ray::new(hit_record.p, target - hit_record.p);
+        *attentuation = self.albedo;
+
+        true
     }
 }
