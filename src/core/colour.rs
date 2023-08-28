c8b4625b58549025dbd85bd438aef9272d8b9025
Aniket <aniket.97.mane@gmail.com>
2023-08-28 21:17:55 +0530

    Modified colour function - added logic for scatter

--- src/core/colour.rs
+++ src/core/colour.rs
@@ -1,14 +1,22 @@
 use rand::Rng;
 
+use crate::materials::material::Scatterable;
+
 use super::hittable::{HittableList, Hittable};
 use super::ray::Ray;
 use super::vec3::Vec3;
 
-pub fn colour(ray: &Ray, world: &HittableList) -> Vec3 {
-    if let Some(rec) = world.hit(ray, 0.0, std::f32::MAX) {
-        let target = rec.p + rec.normal + random_in_unit_sphere();
+pub fn colour(ray: &Ray, world: &HittableList, depth: i32) -> Vec3 {
+    if let Some(rec) = world.hit(ray, 0.001, std::f32::MAX) {
+        let mut scattered = Ray::new(Vec3::default(), Vec3::default());
+        let mut attenuation = Vec3::default();
 
-        return colour(&Ray::new(rec.p, target - rec.p), &world) * 0.5;
+        if depth < 50 && rec.material.scatter(ray, &
+            rec, &mut attenuation, &mut scattered) {
+            return attenuation * colour(&scattered, &world,  depth + 1);
+        } else {
+            return Vec3::new(0.0, 0.0, 0.0);
+        }
     } else {
         let unit_direction = Vec3::unit_vector(&ray.direction());
 
