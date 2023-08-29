8aa252fa32fd3a72c3d2f2bc401cf22bef38d07f
Aniket <aniket.97.mane@gmail.com>
2023-08-29 20:21:17 +0530

    Changes for dielectric

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rand::Rng;
 use ray_tracer::core::hittable::{HittableList, List};
 use ray_tracer::core::sphere::Sphere;
 use ray_tracer::core::{vec3::Vec3, colour::colour};
+use ray_tracer::materials::dielectric::Dielectric;
 use ray_tracer::materials::lambertian::Lambertian;
 use ray_tracer::materials::material::Material;
 use ray_tracer::materials::metal::Metal;
@@ -10,23 +11,24 @@ use ray_tracer::setup::camera::Camera;
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
 
-    let width = 720;
+    let width = 480;
     let height = width as f32 / aspect_ratio;
     let max = 255;
     let samples = 100;
     let mut rng = rand::thread_rng();
 
     let camera = Camera::new();
-    let material1 = Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.3, 0.3)));
-    let material2 = Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)));
-    let material3 = Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.5));
-    let material4 = Material::Metal(Metal::new(Vec3::new(0.8, 0.8, 0.8), 0.0));
+    let material_ground = Material::Lambertian(Lambertian::new(Vec3::new(0.8, 0.8, 0.0)));
+    let material_center = Material::Lambertian(Lambertian::new(Vec3::new(0.1, 0.2, 0.5)));
+    let material_left = Material::Dielectric(Dielectric::new(1.5));
+    let material_right = Material::Metal(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.0));
 
     let mut list: List = Vec::new();
-    list.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, material1)));
-    list.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material2)));
-    list.push(Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material3)));
-    list.push(Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material4)));
+    list.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
+    list.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, material_center)));
+    list.push(Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    list.push(Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), -0.4, material_left)));
+    list.push(Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material_right)));
     let world = HittableList::new(list);
 
     // println!("{} : {}", width, height);
