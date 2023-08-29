97a35de5e0ef086ea68fdd7c4a16f59bbd0be6a7
Aniket <aniket.97.mane@gmail.com>
2023-08-29 19:43:34 +0530

    Implemented Scatterable for Dielectric and added refract function

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -1,2 +1,42 @@
+use crate::core::{ray::Ray, hittable::HitRecord, vec3::Vec3};
+
+use super::material::{Scatterable, refract, reflect};
+
 #[derive(Debug, Clone, Copy)]
-pub struct Dielectric {}
+pub struct Dielectric {
+    pub ir: f32
+}
+
+impl Dielectric {
+    pub fn new(ir: f32) -> Self {
+        Self { ir }
+    }
+}
+
+impl Scatterable for Dielectric {
+    fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
+        *attenuation = Vec3::new(1.0, 1.0, 1.0);
+        let refraction_ratio = if hit_record.front_face {
+            1.0 / self.ir
+        } else {
+            self.ir
+        };
+
+        let unit_direction = Vec3::unit_vector(&ray_in.direction());
+        let cos_theta = Vec3::dot_product( &(-unit_direction), &hit_record.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let mut direction = Vec3::default();
+
+        if cannot_refract {
+            direction = reflect(&unit_direction, &hit_record.normal);
+        } else {
+            direction = refract(&unit_direction, &hit_record.normal, refraction_ratio);
+        }
+
+        *scattered = Ray::new(hit_record.p, direction);
+
+        true
+    }
+}
--- src/materials/material.rs
+++ src/materials/material.rs
@@ -1,6 +1,6 @@
 use crate::core::{ray::Ray, hittable::HitRecord, vec3::Vec3};
 
-use super::{lambertian::Lambertian, metal::Metal, dielectric::Dielectric};
+use super::{lambertian::Lambertian, metal::Metal, dielectric::{Dielectric, self}};
 
 
 #[derive(Clone, Copy)]
@@ -24,6 +24,15 @@ pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     *v - *n * (2.0 * Vec3::dot_product(v, n))
 }
 
+pub fn refract(uv: &Vec3, n: &Vec3, etai_over_etat: f32) -> Vec3 {
+    let cos_theta = Vec3::dot_product(&(-*uv), n).min(1.0);
+    
+    let r_out_perp = (*uv + *n * cos_theta) * etai_over_etat;
+    let r_out_parallel = *n * (-1.0 * (1.0 - r_out_perp.length_squared()).abs().sqrt());
+
+    r_out_perp + r_out_parallel
+}
+
 impl Scatterable for Material {
     fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
         match self {
@@ -33,7 +42,9 @@ impl Scatterable for Material {
             Material::Metal(metal) => {
                 metal.scatter(ray_in, hit_record, attenuation, scattered)
             },
-            Material::Dielectric(_) => todo!(),
+            Material::Dielectric(dielectric) => {
+                dielectric.scatter(ray_in, hit_record, attenuation, scattered)
+            },
         }
     }
 }
